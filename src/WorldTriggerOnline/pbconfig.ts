0fef07d056e1c9bca376ca147fd71c336fad0e46
pic0meter <markpatrickvcastillo@gmail.com>
2026-08-18 01:12:53 +0800

    Update pbconfig.ts

--- src/WorldTriggerOnline/pbconfig.ts
+++ src/WorldTriggerOnline/pbconfig.ts
@@ -3,10 +3,10 @@ import { ContentRating, SourceInfo } from '@paperback/types'
 export const WorldTriggerOnlineInfo: SourceInfo = {
   version: '1.0.0',
   name: 'World Trigger Online',
-  icon: 'icon.png',
-  author: 'YourName',
-  authorWebsite: 'https://github.com',
-  description: 'Extension for world-trigger-chapters.online',
+  icon: 'icon.png', // Add a 512x512 icon.png in the source folder if desired
+  author: 'pic0meter',
+  authorWebsite: 'https://github.com/pic0meter',
+  description: 'Extension to read World Trigger from world-trigger-chapters.online',
   contentRating: ContentRating.EVERYONE,
   websiteBaseURL: 'https://world-trigger-chapters.online'
 }
