aa9de42aba57a119b0ae36b0c607db445358032b
pic0meter <markpatrickvcastillo@gmail.com>
2026-08-18 00:51:05 +0800

    Create pbconfig.ts

---
+++ src/WorldTriggerOnline/pbconfig.ts
@@ -0,0 +1,12 @@
+import { ContentRating, SourceInfo } from '@paperback/types'
+
+export const WorldTriggerOnlineInfo: SourceInfo = {
+  version: '1.0.0',
+  name: 'World Trigger Online',
+  icon: 'icon.png',
+  author: 'YourName',
+  authorWebsite: 'https://github.com',
+  description: 'Extension for world-trigger-chapters.online',
+  contentRating: ContentRating.EVERYONE,
+  websiteBaseURL: 'https://world-trigger-chapters.online'
+}
